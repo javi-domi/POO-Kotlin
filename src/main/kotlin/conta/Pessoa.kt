4c35a1c8b9938877d969695cd85d2cbeea704d5d
Javier Dominguez <javdominguez@icloud.com>
2021-06-30 12:55:07 -0300

    updated to apply private modifier

--- src/main/kotlin/conta/Pessoa.kt
+++ src/main/kotlin/conta/Pessoa.kt
@@ -1,12 +1,19 @@
 package conta
 
 class Pessoa {
-    val nome: String = "Javier"
-    val cpf: String = "066.029.237-81"
+    var nome: String = "Javier"
+    var cpf: String = "066.029.237-81"
+    private set
+
+    inner class Enderco {
+        var rua: String = "Rua Real"
+    }
 }
 
 fun main() {
     val javier = Pessoa()
     println("Nome: " + javier.nome )
     println("CPF: " + javier.cpf )
+    println("Endereço: " + javier.Enderco().rua)
 }
+
