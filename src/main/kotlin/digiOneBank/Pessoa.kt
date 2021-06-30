9871eac8f16ddd6db20ac9e164ffc7e0d4ac015a
Javier Dominguez <javdominguez@icloud.com>
2021-06-30 13:26:02 -0300

    class: created conta.kt, package: changed name to digiOneBank

--- src/main/kotlin/digiOneBank/Pessoa.kt
+++ src/main/kotlin/digiOneBank/Pessoa.kt
@@ -1,4 +1,4 @@
-package conta
+package digiOneBank
 
 class Pessoa {
     var nome: String = "Javier"
---
+++ src/main/kotlin/digiOneBank/conta.kt
@@ -0,0 +1,18 @@
+package digiOneBank
+
+import java.math.BigDecimal
+
+class conta (
+
+    val agencia: String,
+    val numero: String,
+    val saldo: BigDecimal
+    ){
+
+    fun deposito(valor: BigDecimal){
+
+    }
+    fun saque(valor: BigDecimal){
+
+    }
+}
